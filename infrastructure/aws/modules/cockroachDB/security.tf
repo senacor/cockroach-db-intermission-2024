4723630b48227991b60b361aa989bd25e6450f8b
ngocson <son87.lengoc@gmail.com>
2024-06-14 17:55:07 +0200

    feat: added rule for outgoing https requests

--- infrastructure/aws/modules/cockroachDB/security.tf
+++ infrastructure/aws/modules/cockroachDB/security.tf
@@ -50,3 +50,12 @@ resource "aws_vpc_security_group_egress_rule" "db_console" {
   ip_protocol = "TCP"
   tags = local.default_tags
 }
+
+resource "aws_vpc_security_group_egress_rule" "https" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4 = "0.0.0.0/0"
+  from_port = 443
+  to_port = 443
+  ip_protocol = "TCP"
+  tags = local.default_tags
+}
